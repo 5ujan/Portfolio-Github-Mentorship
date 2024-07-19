de97e29e1fe613696ca187df5955348824a60bff
Sujan Baskota <sujan.baskota77@gmail.com>
2024-07-19 16:28:17 +0545

    first commit

---
+++ src/data.js
@@ -0,0 +1,12 @@
+const data = {
+  name: "Bishal Panta",
+  githubLink: "https://github.com/bishal0602",
+  email: "MAIL_ADDRESS",
+  hobbies: ["Movies", "Travelling", "Football"],
+  jobTitle: "Software Engineer",
+  degree: "Bachelor's in Computer Engineering",
+  campus: "Pulchowk Campus",
+  location: "Nepal",
+  spokenLanguages: "Nepali,English",
+};
+export default data;
